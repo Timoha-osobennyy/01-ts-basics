4142642901200c2bc46b5025000c00a70f5f14d6
Timoha-osobennyy <141590304+Timoha-osobennyy@users.noreply.github.com>
2025-07-04 12:14:06 +0300

    еішь

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+﻿import { defineConfig } from "vite";
+import path from "path";
+
+export default defineConfig({
+  base: "/01-ts-basics/",
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
