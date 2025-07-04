c5e679f73a4272656e17c9014465746cbee2393b
Timoha-osobennyy <141590304+Timoha-osobennyy@users.noreply.github.com>
2025-07-04 13:31:02 +0300

    tstim

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,23 @@
 ﻿import { defineConfig } from "vite";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   base: "/01-ts-basics/",
   build: {
-    outDir: "docs",  # Изменяем на docs для GitHub Pages
+    outDir: "docs",
     emptyOutDir: true,
     rollupOptions: {
       input: {
         main: path.resolve(__dirname, "index.html")
       }
+    }
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src")
     }
   }
 });
