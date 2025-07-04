4c79094f2dc3bc9e34cffc19a6df5538542b94a5
Timoha-osobennyy <141590304+Timoha-osobennyy@users.noreply.github.com>
2025-07-04 13:10:10 +0300

    TSTIMM

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+﻿import "./task-1";
+import "./task-2";
+import "./task-3";
+import "./task-4";
+import "./task-5";
+import "./task-6";
+import "./task-7";
+import "./task-8";
+
+const outputDiv = document.getElementById("output")!;
+
+function logToHtml(message: string) {
+    const p = document.createElement("p");
+    p.textContent = message;
+    outputDiv.appendChild(p);
+}
+
+const originalConsoleLog = console.log;
+console.log = function(message: string) {
+    logToHtml(message);
+    originalConsoleLog.apply(console, arguments);
+};
+
+console.log("Application initialized");
